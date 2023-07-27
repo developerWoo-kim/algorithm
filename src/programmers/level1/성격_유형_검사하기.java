819e07e87641a5e165a7770a54fb01738ccab2d5
gunwookim <gunwo151@gmail.com>
2023-07-27 22:06:38 +0900

    * HashMap을 이용한 알고리즘 풀이

---
+++ src/programmers/level1/성격_유형_검사하기.java
@@ -0,0 +1,35 @@
+package programmers.level1;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class 성격_유형_검사하기 {
+    public static void main(String[] args) {
+        String solution = solution(new String[]{"TR", "RT", "TR"}, new int[]{7, 1, 3});
+//        String solution = solution(new String[]{"AN", "CF", "MJ", "RT", "NA"}, new int[]{5, 3, 2, 7, 5});
+    }
+    public static String solution(String[] survey, int[] choices) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(int i = 0; i< survey.length; i++) {
+            int value = choices[i];
+
+            if(value > 0 && value < 4) {
+                char ch = survey[i].charAt(0);
+                map.put(ch, map.getOrDefault(ch, 0) + 4 - value);
+            } else if(value > 4) {
+                char ch = survey[i].charAt(1);
+                map.put(ch, map.getOrDefault(ch, 0) + value - 4);
+            }
+
+        }
+
+        return new StringBuilder()
+                .append(map.getOrDefault('R', 0) >= map.getOrDefault('T', 0) ? 'R' : 'T')
+                .append(map.getOrDefault('C', 0) >= map.getOrDefault('F', 0) ? 'C' : 'F')
+                .append(map.getOrDefault('J', 0) >= map.getOrDefault('M', 0) ? 'J' : 'M')
+                .append(map.getOrDefault('A', 0) >= map.getOrDefault('N', 0) ? 'A' : 'N')
+                .toString();
+    }
+}
