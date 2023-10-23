fa4b155fd8e2c9d9fe49a733ecf62e0decb16522
gunwo151@gmail.com <gunwo151@gmail.com>
2023-10-23 23:11:20 +0900

    * 모두 커밋.

---
+++ src/programmers/level1/옹알이_2.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 옹알이_2 {
+    public static void main(String[] args) {
+        solution(new String[]{"aya", "yee", "u", "maa"});
+    }
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        Set<String> strings = new HashSet<>();
+        String[] arr = {"aya", "ye", "woo", "ma"};
+
+        for(String str : babbling) {
+            int index = 0;
+            char ch = str.charAt(index);
+            index = 0;
+        }
+
+        return answer;
+    }
+}
