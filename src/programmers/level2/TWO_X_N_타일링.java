dd788bbf5665794c4c23e314d7e8ceb8be580bc8
gunwookim <gunwo151@gmail.com>
2023-12-12 23:34:46 +0900

    * 2 X n 타일링

--- src/programmers/level2/TWO_X_N_타일링.java
+++ src/programmers/level2/TWO_X_N_타일링.java
@@ -1,4 +1,12 @@
 package programmers.level2;
 
 public class TWO_X_N_타일링 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
 }
